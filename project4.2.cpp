17984d3a121e11fc8e275f4fc353c83bd794ed18
yarslivje <yarslivje@gmail.com>
2025-10-09 10:52:12 +0300

    init commit

---
+++ project4.2.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    cout << boolalpha << (A >= 0) || (B < -2) << noboolalpha << endl;
+    return 0;
+}
