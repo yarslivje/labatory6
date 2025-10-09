3ac340805de32672ad12e513222bfbb7a177b32e
yarslivje <yarslivje@gmail.com>
2025-10-09 10:55:34 +0300

    update commit

--- project4.2.cpp
+++ project4.2.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main() {
     int A, B;
     cin >> A >> B;
+    cout << "Hello World" << endl;
     cout << boolalpha << (A >= 0) || (B < -2) << noboolalpha << endl;
     return 0;
+
 }
