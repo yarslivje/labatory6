af8629550ea297619dc7f513fd95967d49da3340
yarslivje <yarslivje@gmail.com>
2025-11-27 11:09:40 +0200

    Update in pra1

--- project4.2.cpp
+++ project4.2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int A, B;
     cin >> A >> B;
-    cout << "Hello World" << endl;
+    cout << "Hello World123" << endl;
     cout << boolalpha << (A >= 0) || (B < -2) << noboolalpha << endl;
     return 0;
 
